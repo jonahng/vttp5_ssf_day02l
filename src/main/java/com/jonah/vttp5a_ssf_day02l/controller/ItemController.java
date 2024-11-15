64141cd7b79c367d3df2dc65df28b8e52f8a1088
jonahng <nyhj1998@gmail.com>
2024-11-15 11:46:09 +0800

    got a request parameter for items

--- src/main/java/com/jonah/vttp5a_ssf_day02l/controller/ItemController.java
+++ src/main/java/com/jonah/vttp5a_ssf_day02l/controller/ItemController.java
@@ -1,12 +1,14 @@
 package com.jonah.vttp5a_ssf_day02l.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jonah.vttp5a_ssf_day02l.model.Item;
 import com.jonah.vttp5a_ssf_day02l.service.ItemService;
@@ -23,6 +25,18 @@ public class ItemController {
     public String cartItems(Model model){
         List<Item> items = itemService.getItems();
         model.addAttribute("items",items);
+        return "cartlist";
+
+    }
+
+
+    @GetMapping("/filter") //this  is the url format
+    public String filtercartItems(@RequestParam(name = "itemQuantity") String quantity, Model model){ //the name itemQUantity must be same as the GetMapping
+        List<Item> items = itemService.getItems();
+        //url is http://localhost:8080/items/filter?itemQuantity=3
+        List<Item> filterItems = items.stream().filter(a -> a.getQuantity() > Integer.parseInt(quantity)).collect(Collectors.toList());
+
+        model.addAttribute("items",filterItems);
         return "cartlist";
 
     }
